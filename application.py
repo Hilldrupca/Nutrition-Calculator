d590c9aa79e391e2d931bdcd28290ae435914945
Chris-desktop <hilldrupca@gmail.com>
2020-02-24 16:15:56 -0500

    -Added very basic nutrient profiles (further updates needed). -Spinbox values are accurately updated. No longer delayed by one. -Removing ingredients is reflected in tracked nutrients.

--- application.py
+++ application.py
@@ -1,15 +1,16 @@
 from tkinter import *
+from decimal import Decimal
 import nutrition
 import tkinterAutoEntry
 
-connector = nutrition.Connector()
+nut = nutrition.Connector()
 
 #Nutrients to track. See NUTR_DEF.txt in the Data folder for additional nutrients.
 nutrlist = {'208': ('Calories','kcal'), '204': ('Fats','g'), '605': ('Trans fats','g'),
         '606': ('Saturated fats','g'), '645': ('Monounsaturated fats','g'), '646': ('Polyunsaturated fats','g'),
         '601': ('Cholesterol','mg'), '205': ('Carbohydrates','g'), '291': ('Fiber','g'),
         '203': ('Protein','g'), '320': ('Vitamin A','mcg'), '404': ('Thiamin (B1)','mg'),
-        '405': ('Roboflavin (B2)','mg'), '406': ('Niacin (B3)','mg'), '410': ('Pantothenic acid (B5)','mg'),
+        '405': ('Riboflavin (B2)','mg'), '406': ('Niacin (B3)','mg'), '410': ('Pantothenic acid (B5)','mg'),
         '415': ('Vitamin B6','mg'), '435': ('Folate (B9)','mcg'), '418': ('Vitamin B12','mcg'),
         '401': ('Vitamin C','mg'), '324': ('Vitamin D','IU'), '323': ('Vitamin E','mg'),
         '430': ('Vitamin K','mcg'), '421': ('Choline','mg'), '301': ('Calcium','mg'),
@@ -44,8 +45,8 @@ def add():
     
     dbnum = search.selection[0]
     table[grid_row].append(dbnum)
-    nutinfo = connector.nutData(dbnum)
-    temp =  {}
+    nutinfo = nut.nutData(dbnum)
+    temp = {}
     
     #1st and 2nd indexes are nutrient number and value
     for row in nutinfo:
@@ -68,6 +69,7 @@ def delete(Event):
     row[0].grid_forget()
     row[1].config(text='')
     row[1].grid_forget()
+    row[2].selection_clear()
     row[2].grid_forget()
     ingredientdata.pop(row[3])
     row.remove(row[3])
@@ -79,34 +81,86 @@ def delete(Event):
             table[x][1].grid(row=x+2, column=2)
             table[x][2].grid(row=x+2, column=3)
     
+    release(Event)
+
+def release(Event):
+    Event.widget.after(10,update)
+
+def update():
+    fw = []
+    for x in table:
+        if x[1].cget('text'):
+            #Nutrition information in ingredientdata is per 100 grams
+            food = ingredientdata.get(x[3])
+            
+            y = 0
+            for nutr in nutrlist:
+                value = food.get(nutr)
+                if not value:
+                    value = 0
+                value = Decimal(value*int(x[2].get())/100)
+                fw.insert(y, value)
+                y += 1
+
+    for x in range(0,len(current)):
+        if not len(fw):
+            current[x].config(text='0')
+            per[x].config(text='')
+        else:
+            current[x].config(text='{:.1f}'.format(fw[x]))
+            tar = target[x].cget('text')
+            if tar and current[x].cget('text'):
+                tar = Decimal(tar)
+                percent = fw[x]/tar*100
+                per[x].config(text='{:.1f}'.format(percent))
+
 table = []    
 for x in range(0,15):
     button = Button(frame, bitmap='error')
     button.bind('<Button-1>', delete)
     label = Label(frame, width=75, anchor=W)
     spin = Spinbox(frame, width=7, from_=0, to=10000)
+    spin.bind('<ButtonRelease-1>', release)
     temp = [button, label, spin]
     table.append(temp)
 
 addFood = Button(frame, text='Add Food', command=add)
 addFood.grid(row=0, column=3)
 
+profile = []
+with open('Profiles.txt', newline='\n') as file:
+    start = 41
+    end = 74
+    line_number = 0
+    for line in file:
+        if line_number >40 and line_number <75:
+            value = line.split('=').pop().rstrip()
+            profile.append(value)
+        line_number += 1
+            
 
 nutrientlabel = []
 current = []
 unitlabel = []
+target = []
 targetlabel = []
-nut = nutrition.Connector()
+per = []
 rindex = 0
 for x in nutrlist.values():
     rstring = x[0]
-    nutrientlabel.append(Label(frame, width=20, anchor=E, text=x[0], pady=1))
+    nutrientlabel.append(Label(frame, width=20, anchor=E, text=x[0]))
+    current.append(Label(frame, width = 10, text='0', anchor=E))
     unitlabel.append(Label(frame, width=3, text=x[1], anchor=W))
+    target.append(Label(frame, width=10, text=profile[rindex], anchor=E))
     targetlabel.append(Label(frame, width=3, text=x[1], anchor=W))
+    per.append(Label(frame, width=10, anchor=E))
     
     nutrientlabel[rindex].grid(row=rindex+1, column=4)
+    current[rindex].grid(row=rindex+1, column=5)
     unitlabel[rindex].grid(row=rindex+1, column=6)
+    target[rindex].grid(row=rindex+1, column=7)
     targetlabel[rindex].grid(row=rindex+1, column=8)
+    per[rindex].grid(row=rindex+1, column=9)
     rindex += 1
 
 curlabel = Label(frame, width=10, text='Current', anchor=E)
